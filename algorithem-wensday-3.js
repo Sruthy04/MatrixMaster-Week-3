bc8d6478a0665c6d9f5a78699543531069461c09
Sruthy <Sruthysusan04@gmail.com>
2024-02-29 10:09:20 +0100

    Algorithem-Wednesday-3

---
+++ algorithem-wensday-3.js
@@ -0,0 +1,17 @@
+//1. Factorial! 
+
+function factorialize(n) {
+    if (n < 0) {
+        return "Factorial is not defined for negative numbers.";
+    } else if (n === 0) {
+        return 1;
+    } else {
+        let factorial = 1;
+        for (let i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+   }
+  
+   console.log(factorialize(5)); 
